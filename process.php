184b77db0883c740a8a95fcabe065e1148fd82d0
JoaoPaulo2107 <161509047+JoaoPaulo2107@users.noreply.github.com>
2025-02-28 18:18:51 -0300

    Formulários com o PHP

---
+++ process.php
@@ -0,0 +1,8 @@
+<?php
+  $nome = $_POST['cxnome'];
+  $mensagem = $_POST['cxmsg'];
+
+echo "Bem vindo ", $nome , ",  recebi sua mensagem e sua mensagem é :", $mensagem;
+
+
+?>
---
+++ processa01.php
@@ -0,0 +1,7 @@
+<?php
+  $nome = $_POST['cxnome'];
+
+  echo "Bem vindo ", $nome, "!";
+
+
+?>
