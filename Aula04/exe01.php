e03271cdfd6447faae8711fc87646391120934c7
JoaoPaulo2107 <161509047+JoaoPaulo2107@users.noreply.github.com>
2025-03-07 18:33:53 -0300

    Atividade dia 07/03

---
+++ Aula04/exe01.php
@@ -0,0 +1,11 @@
+<?php
+  $nome = $_GET['cxnome'];
+  $nota1 = $_GET['cxmsg'];
+  $nota2 = $_GET['cxmsg'];
+  $nota3 = $_GET['cxmsg'];
+
+    $media = $n1 + $n2 + $n3/3
+
+echo "Olá", $nome , ", sua média é ", $media;
+
+?>
---
+++ Exercício3dia1701.php
@@ -0,0 +1,18 @@
+<?php
+$n1=5;
+$n2=2;
+
+$adição=$n1+$n2;
+$subtração=$n1-$n2;
+$divisão=$n1/$n2;
+$multiplicação=$n1 * $n2;
+$resto=$n1%$n2;
+
+echo "O resultado da Adição é: " . $adição . "   ";
+echo "O resultado da subtração é: " . $subtração . "   " ;
+echo "O resultado da multiplicação é: " . $multiplicação . "   ";
+echo "O resultado da divisão é: " . $divisão . "   " ;
+echo "O resto da divisão é: ". $resto
+
+
+?>
